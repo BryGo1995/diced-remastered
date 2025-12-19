967fe99ebebb0546469aaf0be7581cd608b1c121
Bryan_G <bryanj.gonzales95@gmail.com>
2025-12-18 22:54:32 -0700

    Add a game board for aligning dice on screen

--- scenes/game_board.gd
+++ scenes/game_board.gd
@@ -1,30 +1,70 @@
 extends Node2D
 
-@export var columns = 0
-@export var rows = 0
-var node_positions := PackedVector2Array()
+@export var columns = 5
+@export var rows = 4
+@export var tile_size := Vector2(66, 66)
+@export var tile_spacing := Vector2(20, 4)
+
+var board := Rect2()
+var board_color = Color(0.0, 0.3, 0.2)
+
+var tile_color = Color(0.3, 0.0, 0.3)
+var tile_positions := PackedVector2Array()
+var tile_centers := PackedVector2Array()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var viewport_dimensions: Vector2 = get_viewport().size
+	set_board_dimensions()
 	
-	var children: Array = get_children()
-	pass # Replace with function body.
+	calculate_tile_positions()
+	calculate_tile_centers()
+	set_child_node_positions()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+	
+func _draw():
+	# Draw the board
+	draw_rect(board, board_color)
+	# Draw the tiles
+	var tile := Rect2()
+	tile.size = tile_size
+	for p in tile_positions:
+		tile.position = p
+		draw_rect(tile, tile_color)
 
-func set_dimensions(width, height):
-	pass
+
+# Calculate the board dimensions based on the size and spacing
+func set_board_dimensions():
+	var board_dimensions = Vector2()
+	board_dimensions.x = columns * tile_size.x + ((columns + 1) * tile_spacing.x)
+	board_dimensions.y = rows * tile_size.y + ((rows + 1) * tile_spacing.y)
+	board.size = board_dimensions
+	print(board)
+
 
 # Based on the number of child nodes, we want to make an informed decision on how many columns/rows
 # there should be.
-func calculate_node_positions():
+func calculate_tile_positions():
 	var pos: Vector2
-	for i in range(get_child_count()):
+	for i in range(rows*columns):
+		pos.x = tile_spacing.x + (i % columns)*((board.size.x - tile_spacing.x)/columns)
+		pos.y = tile_spacing.y + (i / columns)*((board.size.y - tile_spacing.y)/rows)
+		tile_positions.push_back(pos)
 		
 		
-	node_positions.push_back()
-	pass
+func calculate_tile_centers():
+	var pos: Vector2
+	for t in tile_positions:
+		pos = t + tile_size/2
+		tile_centers.push_back(pos)
+		
+	
+func set_child_node_positions():
+	var index = 0
+	for child in get_children():
+		child.position = tile_centers[index]
+		index += 1
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,10 @@
-extends Node
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var viewport_rect: Rect2 = get_viewport_rect()
+	$GameBoard.position = viewport_rect.size / 2 - $GameBoard.board.size/2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +15,4 @@ func _process(delta):
 func roll_all_dice():
 	var dice_group = get_tree().get_nodes_in_group("Dice")
 	for d in dice_group:
-		d.randomize_value()
+		d.randomize_value() 
