251533e20c84d77e229a81e1fe79a5e78f0419d6
Bryan_G <bryanj.gonzales95@gmail.com>
2026-02-24 20:27:18 -0700

    Use onready vars for child nodes in main

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@onready var board_map: Node2D = $BoardMap
+@onready var dice_container: Node2D = $DiceContainer
+@onready var hud: Control = $UI/HUD
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var viewport_rect: Rect2 = get_viewport_rect()
-	$BoardMap.position.x = viewport_rect.size.x/2 - ($BoardMap.map_size_pixels.x/2)
+	board_map.position.x = viewport_rect.size.x/2 - (board_map.map_size_pixels.x/2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,16 +18,16 @@ func _process(delta):
 
 
 func update_roll_button_status():
-	var button_enabled = $BoardMap.is_tile_selected()
+	var button_enabled = board_map.is_tile_selected()
 	if button_enabled:
-		$UI/HUD.enable_roll_button()
+		hud.enable_roll_button()
 	else:
-		$UI/HUD.disable_roll_button()
+		hud.disable_roll_button()
 
 
 func roll_all_dice():
-	$DiceContainer.roll_all_dice()
-	$BoardMap.update_tiles_status()
+	dice_container.roll_all_dice()
+	board_map.update_tiles_status()
 
 
 func calculate_selected_score():
