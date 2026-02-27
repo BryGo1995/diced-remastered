cfda50a72ce01dc1100eb21cceb753dbbd973313
Bryan_G <bryanj.gonzales95@gmail.com>
2026-02-26 18:02:25 -0700

    Connect the main menu play button to the main game scene

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,11 +1,16 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+@onready var play_button: Button = $VBoxContainer/StartButton
+
+
 func _ready():
-	pass # Replace with function body.
+	play_button.pressed.connect(_on_start_button_pressed)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
